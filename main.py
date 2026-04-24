9163a13bf328ed6782f528c53b0a7767efe85885
Reinis <52111482+bad-id@users.noreply.github.com>
2026-04-24 15:43:42 +0200

    PYNQ class

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import pyvisa
 
-ip = "10.43.0.1"
-port = "11008"
-
 defaultPulsePin = "RBPI07"
 defaultDirPin = "RBPI29"
 defaultEndSwitch1Pin = "RBPI27"
 defaultEndSwitch2Pin = "RBPI28"
 defaultPWMDevice = "PWM0"
 
-rm = pyvisa.ResourceManager()
-inst = rm.open_resource(f'TCPIP::{ip}::{port}::SOCKET',
+class PYNQ:
+    def __init__(self,
+                ip:str = "10.43.0.1", 
+                port = "11008"):
+        self.ip = ip
+        self.port = port
+
+        self.rm = pyvisa.ResourceManager()
+        self.inst = self.rm.open_resource(f'TCPIP::{ip}::{port}::SOCKET',
                         open_timeout=10000)
-inst.timeout = 2000
-inst.write_termination = '\n'
-inst.read_termination = '\n'
 
-inst.clear()
-inst.flush(pyvisa.constants.BufferOperation.discard_read_buffer)
-idn = inst.query('*IDN?')
 
-print(idn)
+        self.inst.timeout = 2000
+        self.inst.write_termination = '\n'
+        self.inst.read_termination = '\n'
+        
+        self.inst.clear()
+        self.inst.flush(pyvisa.constants.BufferOperation.discard_read_buffer)
+        
+        self.idn = self.inst.query('*IDN?')
+
+pynq = PYNQ()   
+
+print(pynq.idn)
