2e0c8020802bb2726c9ddbcbd05edeaf9961e372
Tom Verhees <t.f.verhees@student.tue.nl>
2026-05-01 15:23:14 +0200

    entered logs and started writing for autofocus code

--- backend/autofocus.py
+++ backend/autofocus.py
@@ -1,2 +1,19 @@
-import camera
-import pynq
+from camera import Camera
+from pynq import PYNQ
+
+def autofocus(wanted_sharpness):
+    #initiate camera and PYNQ object
+    pynq = PYNQ()
+    camera = Camera()
+    #initialise frequentie
+    #get an image
+    image = camera.get_frame()
+    #get a sharpness value
+    focus_value = camera.check_sharpness(image)
+    while focus_value < wanted_sharpness:
+        
+        PYNQ.startMoving(Direction)
+
+
+
+
