4ce8cf31406fd38602e5320aa05568bd031e3504
Ivan Juric <i.g.juric@hotmail.com>
2026-05-22 16:31:21 +0200

    added functionality to test tenengrad with and without gaussian blur.

--- backend/main.py
+++ backend/main.py
@@ -12,14 +12,8 @@ if __name__ == '__main__':
 
         #pynq.moveToZero()
         #pynq.moveRelativeSteps(10000)
-<<<<<<< HEAD
         pynq.moveAbsoluteSteps(24950)
         print('Stopped')
-=======
-        pynq.moveAbsoluteSteps(10000)
-        pynq.moveAbsoluteSteps(5000)
-        logging.info('Stopped')
->>>>>>> 809209688b1c03d03ae64ba313d60fa6eee4ea35
     
     else:
         logging.error("Error connecting")
